ea890bc1d000d7bd7de37d376d6574a08bc4cad8
Chevy Lin <chevylin0802@gmail.com>
2020-07-01 10:39:13 +0800

    first commit

---
+++ JitsiMeetSample/src/main/java/tw/com/munditv/jitsimeetsample/MainActivity.java
@@ -0,0 +1,55 @@
+package tw.com.munditv.jitsimeetsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize default options for Jitsi Meet conferences.
+        URL serverURL;
+        String str = getString(R.string.default_utl);
+        try {
+            serverURL = new URL(str);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid server URL!");
+        }
+        JitsiMeetConferenceOptions defaultOptions
+            = new JitsiMeetConferenceOptions.Builder()
+                .setServerURL(serverURL)
+                .setWelcomePageEnabled(false)
+                .build();
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+    }
+
+    public void onButtonClick(View v) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+
+        if (text.length() > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            JitsiMeetConferenceOptions options
+                = new JitsiMeetConferenceOptions.Builder()
+                    .setRoom(text)
+                    .build();
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(this, options);
+        }
+    }
+}
